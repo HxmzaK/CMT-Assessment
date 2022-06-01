e3ba974be950db1b5dc611e499cf5dcc1ce137f2
HxmzaK <84510605+HxmzaK@users.noreply.github.com>
2022-06-01 18:01:28 -0400

    Add files via upload

---
+++ CMT Assessment.sql
@@ -0,0 +1,26 @@
+-- !preview conn=DBI::dbConnect(RSQLite::SQLite())
+
+-- CMT Assessment, Hamza Khalid
+-- Query for all drives by user Ken Miles
+
+SELECT u.name, format(d.trip_start_ts, 'yyyy-MM-dd-HHmm') --https://stackoverflow.com/questions/31463457/how-to-convert-getdate-to-yyyy-mm-dd-hhmm-format
+FROM Users u
+JOIN Drives d ON u.id = d.user_id
+WHERE u.name = 'Ken Miles';
+
+/* What changes would I make to this schema?
+To the users table I would add the following fields:
+Demographic Information like location, date of birth, phone number, email address
+Customer Information like insurance policy # and expiration date, credit card information, coverage
+
+To the Drives table I would add the following fields specific to a company utilizing CMT products:
+Amount of times speeding, amount of phone motion, amount of screen interactions, amount of hard brakes, amount of harsh accelerations
+
+I would add the following Claims table with example fields such as: 
+ID, user_id, amount, claim status
+
+Other possible tables added to this schema could be:
+Policy table, Insurance Agent table
+
+Improving the Query Results?
+
